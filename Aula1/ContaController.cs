00106693722ef8c50f590c876f8737a5cee8745a
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-11-06 21:06:44 -0300

    Add project files.

---
+++ Aula1/ContaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula1
+{
+    public class ContaController : ControllerBase
+    {
+        // Qual a Rota que essa Action responde?
+        [Route("/saldo/{cpf}")]
+        public IActionResult GetSaldo(string cpf)
+        {
+            if (cpf == "12345678900")
+            {
+                return Ok("Saldo: R$ 1.000,00");
+            }
+            else
+            {
+                return Ok("Saldo: R$ 0,00");
+            }
+        }
+    }
+}
---
+++ Aula1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder();
+            // Adiciona na Injeção de Dependência
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
