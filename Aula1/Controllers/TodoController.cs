bc6bf1b3553b870e0a8e677ffe99dd7fc8ccebb2
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-11-06 21:41:53 -0300

    Início do Todo API

---
+++ Aula1/Controllers/TodoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula1.Controllers
+{
+    public class TodoController : ControllerBase
+    {
+        // GET: api/todo
+        [Route("api/todo")]
+        public IActionResult GetTodos() // Retorne TODAS as TODOS
+        {
+            return Ok(new[] { "Comprar Ovo", "Comprar Leite", "Dar Remédio pra Doguinha" });
+        }
+
+        // GET: api/todo
+        [Route("api/todo/{id}")]
+        public IActionResult GetById(int id) // Retorne TODAS as TODOS
+        {
+            return Ok(new[] { "Comprar Ovo", "Comprar Leite", "Dar Remédio pra Doguinha" });
+        }
+    }
+}
--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -8,15 +8,11 @@ namespace Aula1
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder();
-            // Adiciona na Injeção de Dependência
             builder.Services.AddControllers();
-            builder.Services.AddRazorPages();
-            builder.Services.AddSignalR();
 
             var app = builder.Build();
             app.MapControllers();
-            app.MapRazorPages();
-            app.MapHub<ChatHub>("/chathub");
+
             app.Run();
         }
     }
